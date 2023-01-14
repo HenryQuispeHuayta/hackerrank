6c20262c384f119dd6b1c1035b1adbf67909d116
Henry Quispe Huayta <henry.quispe.huayta@gmail.com>
2023-01-13 20:16:56 -0400

    grading students

--- week02/GradingStudents.java
+++ week02/GradingStudents.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class GradingStudents {
   public static void main(String[] args) {
     List<Integer> result = new ArrayList<Integer>();
-    result.addAll(Arrays.asList(73,67,38,33));
+    result.addAll(Arrays.asList(73, 67, 38, 33));
     System.out.println(GradingStudentsResult.grandingStudents(result));
   }
 }
@@ -22,14 +22,13 @@ class GradingStudentsResult {
     for (int i = 0; i < grades.size(); i++) {
       int value = grades.get(i);
       for (int j = 1; j < 3; j++) {
-        int newValue = value+j;
-        if(newValue%5==0 && value>37)
+        int newValue = value + j;
+        if (newValue % 5 == 0 && value > 37)
           grades.set(i, newValue);
-
       }
       // int mod = value%5;
       // if (value > 37 && mod < 3) {
-        // grades.set(i, mod);
+      // grades.set(i, mod);
       // }
     }
 
